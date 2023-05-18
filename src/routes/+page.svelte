917140b460393b0aa0bd68daeab42da6a3f57ee1
August <augustwiesner16@gmail.com>
2023-05-17 21:42:35 -0500

    Styled login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,12 @@
     /* CSS Reset, taken from https://www.joshwcomeau.com/css/custom-css-reset/ */
     *, *::before, *::after {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
     } * {
     margin: 0;
-    } html, body {
-        height: 100%;
-    } body {
+    } body, main { min-height: 100vh;} 
+    body {
         line-height: 1.5;
         -webkit-font-smoothing: antialiased;
     } img, picture, video, canvas, svg {
@@ -23,11 +24,42 @@
         overflow-wrap: break-word;
     }
 
+
+    main  {
+        background: linear-gradient(rgba(0,0,0,.6), rgba(0,0,0,.6)), url('../images/pexels-photo-450055.jpeg');
+    
+  background-size: contain;
+        -webkit-animation: slidein 50s;
+        animation: slidein 50s;
+
+        -webkit-animation-fill-mode: forwards;
+        animation-fill-mode: forwards;
+
+        -webkit-animation-iteration-count: infinite;
+        animation-iteration-count: infinite;
+
+        -webkit-animation-direction: alternate;
+        animation-direction: alternate;              
+}
+
+@-webkit-keyframes slidein {
+from {background-position: top; background-size:3000px; }
+to {background-position: -100px 0px;background-size:2750px;}
+}
+
+@keyframes slidein {
+from {background-position: top;background-size:3000px; }
+to {background-position: -100px 0px;background-size:2750px;}
+
+}
+
+    
+
 </style>
 
 <main>
-    <h1>Hello there!</h1>
-    <Home/>
+    <!-- <h1>Hello there!</h1>
+    <Home/> -->
     <Login/>
 </main>
 
--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,38 +1,152 @@
 
 <style lang="scss">
-    form {
-        border: 2px solid black;
+    @import url('https://fonts.googleapis.com/css2?family=Merriweather&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap');
 
-        input {
-            background: none;
-            border: none;
-            outline: none;
-            border: 8px solid red;
-            padding: 1rem;
-        }
+    * {
+      font-family: 'Josefin Sans', sans-serif;
+    }
 
-        button {
-            border: none;
-            outline: none;
-            color: rgb(16, 15, 15);
-            background-color: red;
-            font-size: 1.5rem;
-            padding: 1rem;
-            transition: 0.3s ease;
+    $color: rgb(171, 49, 49);
+    $bg1: #1f2933;
+    
+    .login-main {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        
+        $padding: 4rem;
+        form {
+            background-color: $bg1;
+            box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+            transition: 0.3s ease-in-out;
+            padding: calc($padding * 1);
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            
+            gap: 1.5rem;
+            // &:hover{
+            //     box-shadow: 0px 7.5px 15px 5px rgba(0, 0, 0, 0.5);
+            //     transform: translateY(-5px);
+            // }
 
-            &:active {
-                background-color: pink;
+            h1 {
+              color: white;
+              span {
+                color: $color;
+              }
+            }
+            button {
+                border: none;
+                outline: none;
+                color: rgb(255, 255, 255);
+                background-color: $color;
+                font-size: 1.25rem;
+                padding: 0.75rem 1.75rem;
+                transition: 0.3s ease;
+                border-radius: 9999px;
+                margin-top: 1rem;
+                max-width: fit-content;
+                outline: 3px solid $color;
+
+                &:hover {
+                    background-color: $bg1;
+                    
+                    
+                }
+
+                &:active {
+                    transform: translateY(-5px);
+                   
+                }
             }
         }
     }
+
+// ============================== //
+// INPUT SECTION (NOT HOMEMADE)//    
+// ============================== //
+$bg-color: #2e2e2e;
+$hl-color: $color;
+$muted-color: mix(white, $bg-color, 70%);
+$trans-time: 300ms;
+$width: 100%; .group {position: relative;}
+textarea {resize: none;}
+input, textarea {
+  background: none;
+  color: $muted-color;
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: $width;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid $muted-color;
+  &:focus {
+    outline: none;
+  }&:focus ~ label,&:valid ~ label {
+    top: -14px;
+    font-size: 12px;
+    color: $hl-color;
+  }&:focus ~ .bar:before {
+    width: $width;
+  }
+}input[type="password"] {
+  letter-spacing: 0.3em;
+}label {
+  color: $muted-color;
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: $trans-time ease all;
+}.bar {
+  position: relative;
+  display: block;
+  width: $width;
+  &:before {
+    content: '';
+    height: 2px;
+    width: 0;
+    bottom: 0px;
+    position: absolute;
+    background: $hl-color;
+    transition: $trans-time ease all;
+    left: 0%;
+  }
+}
+
 </style>
 
 
-<form>
-    <h1>Login</h1>
-    <input class="email" type="email" placeholder="Email: ">
-    <input class="password" type="password" placeholder="Password: ">
-    <button type="submit">
-        Log In
-    </button>
-</form>
+<div class="login-main">
+    <form>
+        <h1>Welcome</h1> 
+        <!-- , <br> to <span>Notari</span> -->
+        <!-- TODO
+            Styling of whole login page
+            Style differences and animations for incorrect password, unformatted email.
+            Submit form to a database and send back a response
+        -->
+        <div class="group">
+            <input type="text" required />
+            
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Email</label>
+            
+        </div>
+        <div class="group">
+            <input type="password" required/><span class="highlight"></span><span class="bar"></span>
+            <label>Password</label>
+        </div>        
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+</div>
